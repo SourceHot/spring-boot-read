758dfc89764ae6ad9e7dce2546c0fb81afc09c2a
huifer <huifer97@163.com>
2020-05-22 14:33:57 +0800

    spring bean

---
+++ source-hot-spring-boot/src/main/java/org/sourcehot/controller/DemoController.java
@@ -0,0 +1,10 @@
+package org.sourcehot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+}
